b2326c7cfafe7bea60e2bc53fddb809048e8576b
Tiago Pace <tiagopace.dev@gmail.com>
2025-06-03 17:43:49 -0300

    refact: improve chat message handling

--- app/routes/api.chat.ts
+++ app/routes/api.chat.ts
@@ -1,24 +1,24 @@
+import { createChatMessages, getChatCompletions } from '~/services/chat.server'
+
 import { ChatMessageRole } from '~/generated/prisma'
 import type { Route } from './+types/api.chat'
-import { getChatCompletions } from '~/services/openai.server'
 import prisma from 'prisma/prisma'
 import { redirect } from 'react-router'
 
 export async function action({ request }: Route.ActionArgs) {
   const formData = await request.formData()
-
-  const userMessage = formData.get('message') as string
+  const userInput = formData.get('message') as string
   const chatId = formData.get('chatId') as string
 
-  const chatMessage = {
-    content: userMessage,
+  const userMessage = {
+    content: userInput,
     role: ChatMessageRole.user,
   }
 
   let chat
 
   if (chatId) {
-    const existingChat = await prisma.chat.findUnique({
+    chat = await prisma.chat.findUnique({
       where: {
         id: chatId,
       },
@@ -27,25 +27,17 @@ export async function action({ request }: Route.ActionArgs) {
       },
     })
 
-    if (existingChat) {
-      const answer = {
-        content: (await getChatCompletions([...existingChat.messages, chatMessage])) ?? '',
+    if (chat) {
+      const assistantMessage = {
+        content: (await getChatCompletions([...chat.messages, userMessage])) ?? '',
         role: ChatMessageRole.assistant,
       }
 
-      await prisma.chatMessage.createMany({
-        data: [
-          {
-            chat_id: existingChat.id,
-            ...chatMessage,
-          },
-          { chat_id: existingChat.id, ...answer },
-        ],
-      })
+      await createChatMessages(chat.id, userMessage, assistantMessage)
     }
   } else {
-    const answer = {
-      content: (await getChatCompletions([chatMessage])) ?? '',
+    const assistantMessage = {
+      content: (await getChatCompletions([userMessage])) ?? '',
       role: ChatMessageRole.assistant,
     }
 
@@ -53,15 +45,7 @@ export async function action({ request }: Route.ActionArgs) {
       data: {},
     })
 
-    await prisma.chatMessage.createMany({
-      data: [
-        {
-          chat_id: chat.id,
-          ...chatMessage,
-        },
-        { chat_id: chat.id, ...answer },
-      ],
-    })
+    await createChatMessages(chat.id, userMessage, assistantMessage)
 
     return redirect(`/task/new?chat=${chat.id}`)
   }
