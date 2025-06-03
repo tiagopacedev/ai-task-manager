c2fa111aebf4b65149f64f4274cd862f553491cd
Tiago Pace <tiagopace.dev@gmail.com>
2025-06-03 15:25:09 -0300

    chore: config deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from '@vercel/react-router/vite'
+import type { Config } from '@react-router/dev/config'
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-} satisfies Config;
+  presets: [vercelPreset()],
+} satisfies Config
